c3af76e04afe60480a97878e5a50d11feede681c
Mirado vm <me.radoou@gmail.com>
2017-10-30 16:48:15 +0300

    remove .lire-suite

--- www.journaldugeek.com.php
+++ www.journaldugeek.com.php
@@ -5,7 +5,8 @@ return array(
         '%.*%' => array(
             'test_url' => 'http://www.jdubuzz.com/2016/07/26/vestige-atlantide-google-earth/',
             'body' => array(
-                '//*[@itemprop="articleBody"]'
+                '//*[@itemprop="articleBody"]',
+                '//*[contains(@class, "lire-aussi")]'
             ),
         ),
     ),
